b57aebf52f63c8f194ba9f683f5cd0ab56864c9a
Phelipe Pereira <phelipe.psouza@senacsp.edu.br>
2025-02-03 15:34:39 -0300

    feat: add new module DailyDiet and its assets

---
+++ src/modules/DailyDiet/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native'
+
+import { Container } from './styles'
+
+export function DailyDiet() {
+  return (
+    <Container>
+      <Text style={{ color: '#fff' }}>DailyDiet works</Text>
+    </Container>
+  )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,19 @@
 import { Header } from '@components/Header'
 import { Toolbar } from '@components/Toolbar'
 import { WeeklyProgress } from '@components/WeeklyProgress'
+import { DailyDiet } from 'src/modules/DailyDiet'
 
 import { Container } from './styles'
 
 export function Home() {
   return (
     <Container>
-      <Toolbar />
+      <Toolbar showBackButton title="Daily Diet" />
 
-      <Header />
-      <WeeklyProgress />
+      {/* <Header />
+      <WeeklyProgress /> */}
+
+      <DailyDiet />
     </Container>
   )
 }
