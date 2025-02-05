6ed35261b44170853739cf2d1eaba8742c87e275
Phelipe Pereira <phelipe.psouza@senacsp.edu.br>
2025-02-05 15:19:13 -0300

    chore: move DailyDiet module to screens folder

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
 import { Header } from '@components/Header'
 import { Toolbar } from '@components/Toolbar'
 import { WeeklyProgress } from '@components/WeeklyProgress'
-import { DailyDiet } from 'src/modules/DailyDiet'
+import { DailyDiet } from '@screens/DailyDiet'
 
 import { Container } from './styles'
 
